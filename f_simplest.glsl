3d1d22b4167774933079d84e9f7d97327ecf1713
Dominika Nowak <dominika.nowak@student.put.poznan.pl>
2024-05-28 22:45:20 +0200

    Zad 9. Implementacja modelu Phonga w oparciu o fragment shader

--- f_simplest.glsl
+++ f_simplest.glsl
@@ -1,9 +1,29 @@
 #version 330
 
+in vec4 iC;
+in vec4 lfarg;
+in vec4 nfarg;
+in vec4 vfarg;
 
 out vec4 pixelColor; //Zmienna wyjsciowa fragment shadera. Zapisuje sie do niej ostateczny (prawie) kolor piksela
-in vec4 iC;
 
 void main(void) {
-	pixelColor = iC;
+    vec4 n = normalize(nfarg);
+    vec4 l = normalize(lfarg);
+    vec4 v = normalize(vfarg);
+
+    float nl = max(dot(n, l), 0.0);
+
+    vec4 La = vec4(0.0, 0.0, 0.0, 1.0);
+    vec4 Ld = vec4(1.0, 1.0, 1.0, 1.0);
+
+    vec4 r = normalize(reflect(-l, n));
+
+    float rv = max(dot(r, v), 0.0);
+    rv = pow(rv, 25.0);
+
+    vec4 Ls = vec4(1.0, 1.0, 1.0, 1.0); 
+    vec4 ks = vec4(1.0, 1.0, 1.0, 1.0); 
+
+    pixelColor = (Ld * iC * nl) + (Ls * ks * rv);
 }
--- v_simplest.glsl
+++ v_simplest.glsl
@@ -11,30 +11,18 @@ in vec4 colour;
 in vec4 normals;
 
 out vec4 iC;
-
+out vec4 lfarg;
+out vec4 nfarg;
+out vec4 vfarg;
 
 void main(void) {
+    iC = colour;
 
     vec4  lp=vec4(0,0,-6,1);
 
-    vec4 l = normalize(V * lp - V * M * vertex);
-    vec4 n = normalize(V * M * normals);
+    lfarg = V * lp - V * M * vertex; 
+    nfarg = V * M * normals;
+    vfarg = -V * M * vertex;
 
-    float nl = max(dot(n, l), 0.0);
-
-    vec4 La = vec4(0.0, 0.0, 0.0, 1.0);
-    vec4 Ld = vec4(1.0, 1.0, 1.0, 1.0);
-
-    vec4 r = normalize(reflect(-l, n));
-    vec4 v = normalize(-V * M * vertex);
-
-    float rv = max(dot(r, v), 0.0);
-    rv = pow(rv, 25.0);
-
-    vec4 Ls = vec4(1.0, 1.0, 1.0, 1.0); 
-    vec4 ks = vec4(1.0, 1.0, 1.0, 1.0); 
-
-    iC = (Ld * colour * nl) + (Ls * ks * rv);
- 
     gl_Position = P * V * M * vertex;
 }
